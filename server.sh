751a47cd780356453eb646b48da0b16061109e01
Alexand Sorokin <bsorokind@gmail.com>
2012-07-24 23:16:36 +0300

    server.sh

---
+++ server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Replace these three settings.
+PROJNAME="codeone-test"
+PROJDIR="/home/alex/projects/$PROJNAME/"
+PIDFILE="$PROJDIR/$PROJNAME.pid"
+SOCKET="$PROJDIR/PROJNAME.sock"
+
+cd $PROJDIR
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+fi
+
+source /home/alex/envs/test_env/bin/activate
+exec python manage.py runfcgi socket=$SOCKET pidfile=$PIDFILE
